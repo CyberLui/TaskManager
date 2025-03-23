3146b86592fc896b83a11cc8f93961e71cd6d545
Evandro Sacramento II <evandro-dev@outlook.com>
2025-03-23 17:05:04 -0300

    UserService and TaskService created

---
+++ src/main/java/com/evtech/taskmanager/services/TaskService.java
@@ -0,0 +1,51 @@
+package com.evtech.taskmanager.services;
+
+
+import com.evtech.taskmanager.entities.Task;
+import com.evtech.taskmanager.repositories.TaskRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> findAll(){
+        return taskRepository.findAll();
+    }
+
+    public Task findById(Long id){
+        Optional<Task> obj = taskRepository.findById(id);
+        return obj.orElseThrow(()-> new ResourceAccessException("Task NOt Found!"));
+    }
+
+    public Task insert(Task obj){
+        return taskRepository.save(obj);
+    }
+
+    public Task update(Long id, Task obj){
+        try{
+            Task entity = taskRepository
+                    .findById(id).orElseThrow(()-> new EntityNotFoundException("TAsk Not Found! " + id));
+
+        updateData(entity, obj);
+        return taskRepository.save(entity);
+        }catch (Exception e){
+            throw new RuntimeException("Update Task Error! " + e.getMessage());
+        }
+    }
+
+    private void updateData(Task entity, Task obj) {
+        entity.setTitle(obj.getTitle());
+        entity.setDescription(obj.getDescription());
+        entity.setStatus(obj.getStatus());
+    }
+    
+}
---
+++ src/main/java/com/evtech/taskmanager/services/UserService.java
@@ -0,0 +1,58 @@
+package com.evtech.taskmanager.services;
+
+import com.evtech.taskmanager.entities.User;
+import com.evtech.taskmanager.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(()-> new ResourceAccessException("Id Not Found"));
+    }
+
+    public User insert(User obj){
+        return userRepository.save(obj);
+    }
+
+    public void delete(Long id){
+        userRepository.deleteById(id);
+    }
+
+    public User update(Long id, User obj){
+        try{
+            User entity = userRepository
+                    .findById(id).orElseThrow(() -> new EntityNotFoundException("User Not Found!: " + id));
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (Exception e) {
+            throw new RuntimeException("Update User Error! " + e.getMessage(), e);
+        }
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setUsername(obj.getUsername());
+        entity.setEmail(obj.getEmail());
+        entity.setPassword(obj.getPassword());
+
+
+    }
+
+
+}
