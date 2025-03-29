f824be55a487deacf41983d841462d6c898ad0ed
Evandro Sacramento II <evandro-dev@outlook.com>
2025-03-28 22:55:00 -0300

    UserDTO and TaskDTO created

---
+++ src/main/java/com/evtech/taskmanager/dtos/TaskDTO.java
@@ -0,0 +1,4 @@
+package com.evtech.taskmanager.dtos;
+
+public record TaskDTO(String title, String description) {
+}
---
+++ src/main/java/com/evtech/taskmanager/dtos/UserDTO.java
@@ -0,0 +1,4 @@
+package com.evtech.taskmanager.dtos;
+
+public record UserDTO(String email, String password) {
+}
